6f0eb34bef1410857bb70bb1d6fc55ebf5c11907
Lucian <17691013081@163.com>
2024-11-23 23:30:20 +0800

    2024-11-23： 初始化本地项目

---
+++ src/main/java/org/array/springsecurity/SpringSecurityPractiseApplication.java
@@ -0,0 +1,13 @@
+package org.array.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPractiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityPractiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/array/springsecurity/SpringSecurityPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.array.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPractiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
